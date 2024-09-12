21788300ca506d15a4d4a952ec0f6dab9fa3ed9c
justinslack <justin@nml.co.za>
2024-09-12 10:48:28 +0200

    Add artist collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	type: 'content',
+const artists = defineCollection({
+	type: "content",
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
@@ -13,4 +13,4 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+export const collections = { artists };
